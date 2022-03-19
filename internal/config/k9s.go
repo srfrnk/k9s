398b941fe4932ed94548589c3c810328ee546cc0
Shahar Frank <srfrnk@gmail.com>
2022-03-19 08:48:40 +0000

    refactoring for code quality

--- internal/config/k9s.go
+++ internal/config/k9s.go
@@ -133,12 +133,7 @@ func (k *K9s) IsLogoless() bool {
 
 // ScanForAutoPf returns scanForAutoPf setting.
 func (k *K9s) ShouldScanForAutoPf() bool {
-	h := k.ScanForAutoPf
-	if k.manualScanForAutoPf != nil && *k.manualScanForAutoPf {
-		h = *k.manualScanForAutoPf
-	}
-
-	return h
+	return k.ScanForAutoPf
 }
 
 // IsCrumbsless returns crumbsless setting.
