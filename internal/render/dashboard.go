9ebe5e8f64141c1864ac1b5fb533b62042f8cd24
Shahar Frank <shahar.frank@tesco.com>
2023-10-20 16:26:02 +0100

    fix after merge

--- internal/render/dashboard.go
+++ internal/render/dashboard.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/derailed/k9s/internal/client"
 	"github.com/derailed/k9s/internal/config"
-	"github.com/gdamore/tcell/v2"
+	"github.com/derailed/tcell/v2"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
